87ba0fa9ab00a54baacfe0aea2359d7bba29e2cd
Lavender Awuor <awuor053@gmail.com>
2026-03-17 17:05:52 +0300

    Test failing CI

---
+++ bad_code.py
@@ -0,0 +1,10 @@
+#This is a comment - it is ignored by Python
+#Variables are created by assigning a value
+name = "World"
+greeting = "Hello
+#The print ()function displays output to the console
+print(f"{greeting}, {name}!")
+
+#Simple Loop Example
+for i in range(3):
+print(f"Loop iteration {i}")
